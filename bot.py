3ecef166333130a7db463934c5707b4bcf3c056d
Jammie sandy <33603488+jamessandy@users.noreply.github.com>
2021-07-08 16:09:41 +0100

    Update and rename bot to bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+!/usr/bin/env python
+
+import webbrowser
+import time
+x = 1
+while True:
+  webbrowser.open('https://ghanahookup.onlinepornhub.tk/')
+  time.sleep(15)
+	webbrowser.open_new_tab('https://ghanahookup.onlinepornhub.tk/')
+	time.sleep(10)
+	webbrowser.open_new_tab('https://ghanahookup.onlinepornhub.tk/')
+	time.sleep(10)	
+	webbrowser.open_new_tab('https://ghanahookup.onlinepornhub.tk/')
+	time.sleep(10)
+	webbrowser.open_new_tab('https://ghanahookup.onlinepornhub.tk/')
+	time.sleep(10)	
+	webbrowser.open_new_tab('https://ghanahookup.onlinepornhub.tk/')
+	time.sleep(10)
+	x +=1
+	
+	
